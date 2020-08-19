90207db5af5d262129ae7eea74b327319ef39dfd
shivam saxena <shivam.saxena@squareboat.com>
2020-08-19 19:19:14 +0530

    ADD: Animations transitions

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
-console.log('hello');
+// SCROLL TO TOP
+window.onscroll = function () {
+  scrollFunction();
+};
+
+var scrollTopBtn = document.getElementById("scrollTop");
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    scrollTopBtn.classList.add("active");
+  } else {
+    scrollTopBtn.classList.remove("active");
+  }
+}
+
+scrollTopBtn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
